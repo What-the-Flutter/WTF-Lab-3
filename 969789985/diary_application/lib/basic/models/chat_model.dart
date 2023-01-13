54d95ca72ebc7e921b678ef56df21a6456ae4360
tokyolem <heenssss@mail.ru>
2023-01-13 14:54:03 +0300

    Implemented home screen + bottom nav bar

---
+++ 969789985/diary_application/lib/basic/models/chat_model.dart
@@ -0,0 +1,10 @@
+class ChatModel {
+  int id;
+  String chatTitle;
+  String chatDescription;
+
+  ChatModel({
+    required this.id,
+    required this.chatTitle,
+    required this.chatDescription});
+}
