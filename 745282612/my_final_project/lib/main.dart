c3af9a48584523c698bdfe0bb879a8327f9c005d
Maksim Nevar <syprenevor@mail.ru>
2023-01-22 17:22:21 +0300

    add splash screen

--- 745282612/my_final_project/lib/main.dart
+++ 745282612/my_final_project/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:my_final_project/my_chat_app.dart';
 import 'package:my_final_project/services/auth.dart';
@@ -7,8 +8,10 @@ import 'package:my_final_project/services/auth.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final user = await AuthServise().getOrCreateUser();
+  FlutterNativeSplash.remove();
   runApp(MainApp(user: user));
 }
