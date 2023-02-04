71d6f0b02911bc5dc3976049ed29f4dc30523f1c
Damir Abdulin <damir.abdulin.tel@gmail.com>
2023-02-04 08:46:15 +0300

    Move app class to separate file

---
+++ 680770900/cool_chat_journal/lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page/home_page.dart';
+import 'theme.dart';
+
+class CoolChatJournalApp extends StatelessWidget {  
+  const CoolChatJournalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) { 
+    var theme = AppTheme();
+    return MaterialApp(
+      title: 'Cool Chat Journal',
+      theme: theme.createThemeData(context),
+      home: const HomePage(appName: 'Cool Chat Journal'),
+    );
+  }
+}
--- 680770900/cool_chat_journal/lib/main.dart
+++ 680770900/cool_chat_journal/lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'pages/home_page/home_page.dart';
+import 'app.dart';
 
 void main() {
   runApp(const CoolChatJournalApp());
-}
-
-class CoolChatJournalApp extends StatelessWidget {
-  const CoolChatJournalApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cool Chat Journal',
-      theme: theme,
-      home: const HomePage(appName: 'Cool Chat Journal'),
-    );
-  }
 }
