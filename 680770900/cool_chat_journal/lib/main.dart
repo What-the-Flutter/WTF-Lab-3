d6899b07dac35b590480e136bf92d1ad958db6eb
Damir Abdulin <damir.abdulin.tel@gmail.com>
2023-03-20 11:30:15 +0300

    Create connection with firebase

--- 680770900/cool_chat_journal/lib/main.dart
+++ 680770900/cool_chat_journal/lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final user = await FirebaseAuth.instance.signInAnonymously();
+
   runApp(const CoolChatJournalApp());
 }
